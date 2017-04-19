f024b2a79279564d7e587092094e8a8b6329bf6a
v-kyzhao <v-kyzhao@KYLEZHAO_DESKTO>
2017-04-19 20:30:02 +0800

    Add project files.

---
+++ StressLoadDemo/Model/IStressDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StressLoadDemo.Model
+{
+    public interface IStressDataProvider
+    {
+        Task<int> GetDeviceNumber();
+        Task<int> GetMessageNumber();
+
+    }
+}
---
+++ StressLoadDemo/Model/StressDataMockProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StressLoadDemo.Model
+{
+    public class StressDataMockProvider:IStressDataProvider
+    {
+        private static int querycount = 0;
+        public Task<int> GetDeviceNumber()
+        {
+            return Task.FromResult(querycount++);
+        }
+
+        public Task<int> GetMessageNumber()
+        {
+            return Task.FromResult(2*querycount++);
+        }
+    }
+}
